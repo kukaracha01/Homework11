7cae1c958a4a24f9a4df562571c7f88e1230fc48
mu <semkadarov@gmail.com>
2024-04-29 19:10:04 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int year = new Scanner(System.in).nextInt();
+        System.out.println("Task1");
+        int year = 2023;
         checkLeapYear(year);
+
+        System.out.println("Task2");
+        int clientDeviceYear = 2016;
+        int clientOS = 0;
+        checkClientOSAndDeviceYear(clientDeviceYear, clientOS);
+
     }
+
     public static void checkLeapYear(int yearNew) {
         if (yearNew % 4 == 0 && yearNew % 100 != 0 || yearNew % 400 == 0) {
             System.out.println(yearNew + " год является високосным");
@@ -15,7 +21,28 @@ public class Main {
         }
     }
 
+    public static void checkClientOSAndDeviceYear(int clientDeviceYear, int clientOS) {
+        if (clientDeviceYear >= 2015) {
+            switch (clientOS) {
+                case 0:
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                    break;
+                case 1:
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                    break;
+            }
+        } else {
+            switch (clientOS) {
+                case 0:
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                    break;
+                case 1:
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                    break;
+            }
 
+        }
+        return;
+    }
 }
-
 
