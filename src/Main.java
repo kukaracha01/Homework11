2773255580950d72733241f45ae9d180423e0b1e
mu <semkadarov@gmail.com>
2024-04-29 18:49:11 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int year = new Scanner(System.in).nextInt();
+        checkLeapYear(year);
     }
+    public static void checkLeapYear(int yearNew) {
+        if (yearNew % 4 == 0 && yearNew % 100 != 0 || yearNew % 400 == 0) {
+            System.out.println(yearNew + " год является високосным");
+            return;
+        } else {
+            System.out.println(yearNew + " год является не високосным");
+            return;
+        }
+    }
+
+
 }
+
+
